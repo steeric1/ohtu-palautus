e974007951cbe8868dda65097d43173e77b0651b
Nikko Kujanpää <nikko.kujanpaa@gmail.com>
2022-11-28 20:32:42 +0200

    Viikko 4, tehtävä 5: Kahden eri tuotteen lisäämisen jälkeen ostoskori palauttaa hinnaksi tuotteiden hintojen summan

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -30,3 +30,12 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(leipa)
 
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
+
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_korin_hinta_sama_kuin_tuotteiden_hintojen_summa(self):
+        maito = Tuote("Maito", 3)
+        leipa = Tuote("Leipä", 2)
+        
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(leipa)
+
+        self.assertEqual(self.kori.hinta(), 3 + 2)
