5194bd2ea6acd1456398142788a0e6dcdba34724
Nikko Kujanpää <nikko.kujanpaa@gmail.com>
2022-11-28 20:31:12 +0200

    Viikko 4, tehtävä 5: Ostoskori palauttaa tavaroiden määräksi 2 kahden eri tuotteen lisäämisen jälkeen

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -21,3 +21,12 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
 
         self.assertEqual(self.kori.hinta(), 3)
+
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_korissa_kaksi_tavaraa(self):
+        maito = Tuote("Maito", 3)
+        leipa = Tuote("Leipä", 2)
+        
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(leipa)
+
+        self.assertEqual(self.kori.tavaroita_korissa(), 2)
